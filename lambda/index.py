7799a92a96e483fe0e91fcf83413f3358baaa12f
Koki Fujiya <obabukouki@gmail.com>
2025-04-30 05:08:11 +0000

    FastAPIに接続するようにしました

--- lambda/index.py
+++ lambda/index.py
@@ -4,7 +4,7 @@ import urllib.request
 import urllib.parse
 
 # FastAPIのURL（ngrokのURL）
-FASTAPI_URL = os.environ.get("FASTAPI_URL", "https://d4a9-34-10-132-92.ngrok-free.app/generate")
+FASTAPI_URL = os.environ.get("FASTAPI_URL", "https://08f7-34-34-62-25.ngrok-free.app/generate")
 
 # Lambda コンテキストからリージョンを抽出する関数
 def extract_region_from_arn(arn):
